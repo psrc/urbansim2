83f83a0d368716ae17ee6863b70c3b93d4cdcfa5
Hana Sevcikova <hsevcikova@psrc.org>
2015-05-12 15:35:01 -0700

    out_directory changed

--- data/conversion/inputs.py
+++ data/conversion/inputs.py
@@ -1,6 +1,6 @@
 run = "run_113"
 year = 2010
-out_directory = "output"
+out_directory = "output%s%s" % (year, run)
 output_file = "out%s%s.h5" % (year, run)
 run_directory = os.path.join(os.getenv("OPUS_HOME", "e:/opus"), "data/psrc_parcel/runs", str(run), str(year))
 
