954655b8726eaecfe0c9837a93391dbdf087e8d2
Hana Sevcikova <hsevcikova@psrc.org>
2017-06-13 12:20:32 -0700

    new variables

--- indicators/export_variables.py
+++ indicators/export_variables.py
@@ -6,7 +6,9 @@ import re
 
 @orca.step()
 def export_variables(parcels):
-    attributes = ['building_sqft_pcl', 'residential_units', 'nonres_building_sqft', 'job_capacity', 'number_of_households', 'number_of_jobs', 'land_cost', 'max_dua', 'max_far']
+    attributes = ['building_sqft_pcl', 'residential_units', 'nonres_building_sqft', 'job_capacity', 'land_area', 'parcel_sqft',
+                  'number_of_households', 'number_of_jobs', 'land_cost', 'max_dua', 'max_far', 'land_use_type_id', 'number_of_buildings',
+                  'zone_id', 'faz_id', 'growth_center_id', 'city_id']
     data = {}
     for attr in attributes:
         data[re.sub('_pcl$', '', attr)] = parcels[attr]
