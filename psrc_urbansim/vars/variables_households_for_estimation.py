ded210392f1bfacabb9383b633f1c44fdb98ee75
Stefan Coe <coestefan@gmail.com>
2018-01-09 12:41:31 -0800

    Attempting to fix worker1_zone_id & worker2_zone_id.

--- psrc_urbansim/vars/variables_households_for_estimation.py
+++ psrc_urbansim/vars/variables_households_for_estimation.py
@@ -42,7 +42,7 @@ def income_category(households_for_estimation, settings):
 
 #@orca.column('households_for_estimation', 'is_inmigrant', cache=True)
 #def is_inmigrant(households_for_estimation, parcels):
-#    return (households_for_estimation.building_id < 0).reindex(households_for_estimation.index)
+#    return return (households_for_estimation.building_id < 0).reindex(households_for_estimation.index)
 
 @orca.column('households_for_estimation', 'is_residence_mf', cache=True)
 def is_residence_mf(households_for_estimation, buildings):
@@ -66,20 +66,50 @@ def residence_large_area(households_for_estimation, buildings):
 def tractcity_id(households_for_estimation, parcels):
     return misc.reindex(parcels.tractcity_id, households_for_estimation.parcel_id)
 
+#@orca.column('households_for_estimation', 'worker1_zone_id', cache=True)
+#def worker1_zone_id(households_for_estimation, persons_for_estimation):
+#    return misc.reindex(persons_for_estimation.workplace_zone_id[persons_for_estimation.worker1==True], households_for_estimation.building_id).fillna(-1)
+
 @orca.column('households_for_estimation', 'worker1_zone_id', cache=True)
 def worker1_zone_id(households_for_estimation, persons_for_estimation):
-    return misc.reindex(persons_for_estimation.workplace_zone_id[persons_for_estimation.worker1==True], households_for_estimation.building_id).fillna(-1)
+    df = persons_for_estimation.to_frame()
+    df = df[df.worker1==True]
+    df = df[['workplace_zone_id', 'household_id']]
+    df2 = pd.DataFrame(households_for_estimation.index)
+    df2 = df2.merge(df, how='left', on ='household_id').fillna(-1)
+    df2.set_index(df2.household_id, inplace = True)
+    return df2.workplace_zone_id 
+    
+    #df = persons_for_estimation.to_frame()
+    #df = df[df.worker1==True]
+    #df = df[['workplace_zone_id', 'household_id']]
+    #df.set_index(df.household_id, inplace = True)
+    #df2 = pd.DataFrame(households_for_estimation.index)
+    #return misc.reindex(df.workplace_zone_id, df2.household_id).fillna(-1)
+    
+
+#return misc.reindex(df.workplace_zone_id, pd.DataFrame(households_for_estimation.index).household_id).fillna(-1)
+    #misc.reindex(df.workplace_zone_id, df2.household_id).fillna(-1)
+    #return misc.reindex(df2.household_id, df2.).fillna(-1)
+    #test = pd.merge(pd.DataFrame({"left": df2.household_id}),
+    #              pd.DataFrame({"right": df.workplace_zone_id}),
+    #              left_on="left",
+    #              right_index=True,
+    #              how="left")
 
 @orca.column('households_for_estimation', 'worker2_zone_id', cache=True)
 def worker2_zone_id(households_for_estimation, persons_for_estimation):
-    return misc.reindex(persons_for_estimation.workplace_zone_id[persons_for_estimation.worker2==True], households_for_estimation.building_id).fillna(-1)
+    df = persons_for_estimation.to_frame()
+    df = df[df.worker2==True]
+    df = df[['workplace_zone_id', 'household_id']]
+    df2 = pd.DataFrame(households_for_estimation.index)
+    df2 = df2.merge(df, how='left', on ='household_id').fillna(-1)
+    df2.set_index(df2.household_id, inplace = True)
+    return df2.workplace_zone_id 
+    #return misc.reindex(persons_for_estimation.workplace_zone_id[persons_for_estimation.worker2==True], households_for_estimation.building_id).fillna(-1)
 
 @orca.column('households_for_estimation', 'work_zone_id', cache=True)
 def work_zone_id(households_for_estimation, buildings):
-    return misc.reindex(buildings.zone_id, households_for_estimation.building_id).fillna(-1)
-
-@orca.column('households_for_estimation', 'zone_id', cache=True)
-def zone_id(households_for_estimation, buildings):
     return misc.reindex(buildings.zone_id, households_for_estimation.building_id).fillna(-1)
 
 @orca.column('households_for_estimation', 'zone_id', cache=True)
