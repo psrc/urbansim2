8dc31e34ddb6ca9090400f94b0c114578e4047b7
Stefan Coe <coestefan@gmail.com>
2018-01-12 13:23:20 -0800

    Adding interaction variables for workplace location estimation.

--- psrc_urbansim/vars/variables_interactions.py
+++ psrc_urbansim/vars/variables_interactions.py
@@ -7,7 +7,14 @@ from psrc_urbansim.vars.abstract_variables import abstract_travel_time_interacti
 from psrc_urbansim.vars.abstract_variables import abstract_logsum_interaction_variable
 
 def network_distance_from_home_to_work(work_zones, location_zones):
-    return abstract_travel_time_interaction_variable(orca.get_table("travel_data")["single_vehicle_to_work_travel_distance"], work_zones, location_zones, direction_from_home = False)
+    travel_data = orca.get_table("travel_data")
+    worker = abstract_travel_time_interaction_variable(travel_data["single_vehicle_to_work_travel_distance"], work_zones, location_zones, direction_from_home = False)
+    return worker.values
+
+def generalized_cost_from_home_to_work(work_zones, location_zones):
+    travel_data = orca.get_table("travel_data")
+    worker = abstract_travel_time_interaction_variable(travel_data["single_vehicle_to_work_travel_cost"], work_zones, location_zones, direction_from_home = False)
+    return worker.values
 
 def avg_network_distance_from_home_to_work(work1_zones, work2_zones, location_zones):
     travel_data = orca.get_table("travel_data")
@@ -20,6 +27,7 @@ def avg_network_distance_from_home_to_work(work1_zones, work2_zones, location_zo
     # if there are no workers set it to the regional mean
     fillidx = np.where(np.isnan(res))
     res.iloc[fillidx] = res.mean()
+    #return res.values
     return res.values
 
 def max_logsum_hbw_am_from_home_to_work(work1_zones, work2_zones, location_zones, agent_income_categories):
@@ -43,6 +51,23 @@ def max_logsum_hbw_am_from_home_to_work_wzone_logsum(work1_zones, work2_zones, l
     fillidx = np.where(np.logical_or(np.isnan(res), res < zonal_threshold))
     res[fillidx] = avg_trip.iloc[fillidx]
     return res
+
+def logsum_hbw_am_from_home_to_work(work_zones, location_zones, agent_income_categories):
+    travel_data = orca.get_table("travel_data")
+    tm_dict = {1: travel_data["logsum_hbw_am_income_1"], 2: travel_data["logsum_hbw_am_income_2"], 
+               3: travel_data["logsum_hbw_am_income_3"], 4: travel_data["logsum_hbw_am_income_4"]}
+    worker = abstract_logsum_interaction_variable(tm_dict, agent_income_categories, work_zones, location_zones, direction_from_home = False)
+    return worker
+
+def logsum_hbw_am_from_home_to_work_wzone_logsum(work_zones, location_zones, agent_income_categories, 
+                                                          twa_logsum_1, twa_logsum_2, twa_logsum_3, twa_logsum_4,
+                                                          zonal_threshold=-20):
+    """If max_logsum_hbw_am_from_home_to_work < zonal_threshold, use avg_trip_weighted_zone_logsum."""
+    res = logsum_hbw_am_from_home_to_work(work_zones, location_zones, agent_income_categories)
+    avg_trip = avg_trip_weighted_zone_logsum(agent_income_categories, twa_logsum_1, twa_logsum_2, twa_logsum_3, twa_logsum_4)
+    fillidx = np.where(np.logical_or(np.isnan(res), res < zonal_threshold))
+    res[fillidx] = avg_trip.iloc[fillidx]
+    return res
     
 def avg_trip_weighted_zone_logsum(income_categories, twa_logsum_1, twa_logsum_2, twa_logsum_3, twa_logsum_4):
     return (income_categories == 1)*twa_logsum_1 + (income_categories == 2)*twa_logsum_2 + (income_categories == 3)*twa_logsum_3 + (income_categories == 4)*twa_logsum_4
@@ -55,8 +80,10 @@ def empden_zone_sector(sector, bzone_id):
     zone_density[~np.isfinite(zone_density)] = 0
     return misc.reindex(zone_density, bzone_id)
 
-
 from urbansim.models import dcm
+dcm.network_distance_from_home_to_work = network_distance_from_home_to_work
 dcm.avg_network_distance_from_home_to_work = avg_network_distance_from_home_to_work
 dcm.max_logsum_hbw_am_from_home_to_work_wzone_logsum = max_logsum_hbw_am_from_home_to_work_wzone_logsum
+dcm.logsum_hbw_am_from_home_to_work_wzone_logsum = logsum_hbw_am_from_home_to_work_wzone_logsum
 dcm.empden_zone_sector = empden_zone_sector
+dcm.generalized_cost_from_home_to_work = generalized_cost_from_home_to_work
