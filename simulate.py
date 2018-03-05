d719a28c23c4f398d2bace8f5b5e45e9b691c3ef
Stefan Coe <coestefan@gmail.com>
2018-03-05 10:06:24 -0800

    Current simulation set up.

--- simulate.py
+++ simulate.py
@@ -1,5 +1,6 @@
 import os
 import psrc_urbansim.models
+import psrc_urbansim.workplace_models
 import psrc_urbansim.developer_models
 import orca
 import logging
@@ -28,25 +29,31 @@ def tables_in_base_year():
                                                             "settings.yaml"))['store']), mode="r")
      return [t for t in orca.list_tables() if t in h5store]
 
+
 orca.run([
 #    "add_lag1_tables",
     #"proforma_feasibility",
     #"residential_developer",      # residential proforma model
     #"non_residential_developer",  # non-residential proforma model
+    #"wahcm_estimate",
+    #"delete_invalid_households_persons",
+    #"base_year_wplcm_simulate",
     "update_household_previous_building_id",
     "update_buildings_lag1",
     #"repmres_simulate",          # residential REPM
-    #"repmnr_simulate",            # non-residential REPM
-
+    #"repmnr_simulate",
+            # non-residential REPM
     "households_transition",     # households transition
     "households_relocation",     # households relocation model
-    "hlcm_simulate",            # households location choice
-
+    "hlcm_simulate",
+    #"update_household_parcel_id",
     "jobs_transition",           # jobs transition
-    "jobs_relocation",           # jobs relocation model
+    "jobs_relocation",
+    'update_persons_jobs',           # jobs relocation model
     "elcm_simulate",             # employment location choice
-    "governmental_jobs_scaling"
-
+    "governmental_jobs_scaling",
+    "delete_unplaced_jobs",
+    "wplcm_simulate"
 ], iter_vars=[2015, 2016], data_out=outfile, out_base_tables=tables_in_base_year(),
    compress=True, out_run_local=True)
 
