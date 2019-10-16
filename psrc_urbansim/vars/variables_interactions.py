9a10677024d222476de19d451c49b3233a7c89b6
Hana Sevcikova <hsevcikova@psrc.org>
2019-10-16 11:49:31 -0700

    fixed accessibility variables that returned NaNs

--- psrc_urbansim/vars/variables_interactions.py
+++ psrc_urbansim/vars/variables_interactions.py
@@ -16,33 +16,31 @@ def generalized_cost_from_home_to_work(work_zones, location_zones):
     worker = abstract_travel_time_interaction_variable(travel_data["single_vehicle_to_work_travel_cost"], work_zones, location_zones, direction_from_home = False)
     return worker.values
 
+def set_worker2_to_worker1_if_needed(worker1, worker2):
+    # if worker2 does not work and worker1 does, set it to the same value as worker1 in order for the desired quantity the same as worker1  
+    fillidx = np.where(np.logical_and(np.isnan(worker2), np.isnan(worker1)==False))
+    worker2.iloc[fillidx] = worker1.iloc[fillidx]
+    return worker2
+    
 def avg_network_distance_from_home_to_work(work1_zones, work2_zones, location_zones):
     travel_data = orca.get_table("travel_data")
     worker1 = abstract_travel_time_interaction_variable(travel_data["single_vehicle_to_work_travel_distance"], work1_zones, location_zones, direction_from_home = False)
     worker2 = abstract_travel_time_interaction_variable(travel_data["single_vehicle_to_work_travel_distance"], work2_zones, location_zones, direction_from_home = False)
-    # if worker2 does not work set it to the same value as worker1 in order for the average to be the worker1 distance  
-    fillidx = np.where(np.logical_and(np.isnan(worker2), np.isnan(worker1)==False))
-    worker2.iloc[fillidx] = worker1.iloc[fillidx]
+    worker2 = set_worker2_to_worker1_if_needed(worker1, worker2)
+    worker1 = set_worker2_to_worker1_if_needed(worker2, worker1)
     res = (worker1 + worker2)/2.
-    # if there are no workers set it to the regional mean
-    fillidx = np.where(np.isnan(res))
-    res.iloc[fillidx] = res.mean()
-    #return res.values
-    return res.values
+    # if there are no workers set it to the regional mean; if the mean is na, set it to 0
+    return res.fillna(res.mean()).fillna(0).values
 
 def max_network_distance_from_home_to_work(work1_zones, work2_zones, location_zones):
     travel_data = orca.get_table("travel_data")
     worker1 = abstract_travel_time_interaction_variable(travel_data["single_vehicle_to_work_travel_distance"], work1_zones, location_zones, direction_from_home = False)
     worker2 = abstract_travel_time_interaction_variable(travel_data["single_vehicle_to_work_travel_distance"], work2_zones, location_zones, direction_from_home = False)
-    # if worker2 does not work set it to the same value as worker1 in order for the average to be the worker1 distance  
-    fillidx = np.where(np.logical_and(np.isnan(worker2), np.isnan(worker1)==False))
-    worker2.iloc[fillidx] = worker1.iloc[fillidx]
+    worker2 = set_worker2_to_worker1_if_needed(worker1, worker2)
+    worker1 = set_worker2_to_worker1_if_needed(worker2, worker1)   
     res = pd.concat([worker1, worker2], axis=1).max(axis=1)
-    # if there are no workers set it to the regional mean
-    fillidx = np.where(np.isnan(res))
-    res.iloc[fillidx] = 999999
-    #return res.values
-    return res.values
+    # if there are no workers set it to the regional max or a high number
+    return res.fillna(res.max()).fillna(999999).values
 
 def max_logsum_hbw_am_from_home_to_work(work1_zones, work2_zones, location_zones, agent_income_categories):
     travel_data = orca.get_table("travel_data")
@@ -50,9 +48,8 @@ def max_logsum_hbw_am_from_home_to_work(work1_zones, work2_zones, location_zones
                3: travel_data["logsum_hbw_am_income_3"], 4: travel_data["logsum_hbw_am_income_4"]}
     worker1 = abstract_logsum_interaction_variable(tm_dict, agent_income_categories, work1_zones, location_zones, direction_from_home = False)
     worker2 = abstract_logsum_interaction_variable(tm_dict, agent_income_categories, work2_zones, location_zones, direction_from_home = False)
-    # if worker2 does not work set it to the same value as worker1 in order for the maximum to be the worker1 value  
-    fillidx = np.where(np.logical_and(np.isnan(worker2), np.isnan(worker1)==False))
-    worker2[fillidx] = worker1[fillidx]
+    worker2 = set_worker2_to_worker1_if_needed(worker1, worker2)
+    worker1 = set_worker2_to_worker1_if_needed(worker2, worker1)
     res = np.maximum.reduce([worker1, worker2])
     return res
 
